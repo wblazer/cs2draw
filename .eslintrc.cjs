c9b42b27ce10cdcc9b97e6b7fec17118d65282f6
William Blazer <82427064+wblazer@users.noreply.github.com>
2024-08-18 00:03:29 -0500

    Added tldraw license, started README, and made linting stricter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,18 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
+  },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
