65e5216ce3bbba831522cda5a5308dfd498e2158
William Blazer <82427064+wblazer@users.noreply.github.com>
2024-08-23 21:00:24 -0500

    Hid theme setting menu

--- src/CustomMainMenu.tsx
+++ src/CustomMainMenu.tsx
@@ -1,14 +1,31 @@
 import {
   DefaultMainMenu,
-  DefaultMainMenuContent,
+  EditSubmenu,
+  ExportFileContentSubMenu,
+  ExtrasGroup,
+  KeyboardShortcutsMenuItem,
+  LanguageMenu,
   TldrawUiMenuGroup,
   TldrawUiMenuItem,
+  TldrawUiMenuSubmenu,
+  ToggleEdgeScrollingItem,
+  ToggleFocusModeItem,
+  ToggleGridItem,
+  ToggleReduceMotionItem,
+  ToggleSnapModeItem,
+  ToggleToolLockItem,
+  ToggleWrapModeItem,
+  ViewSubmenu,
 } from "tldraw";
 
 export function CustomMainMenu() {
   return (
     <DefaultMainMenu>
-      <DefaultMainMenuContent />
+      <EditSubmenu />
+      <ViewSubmenu />
+      <ExportFileContentSubMenu />
+      <ExtrasGroup />
+      <CustomPreferencesGroup />
       <div>
         <TldrawUiMenuGroup id="example">
           <TldrawUiMenuItem
@@ -23,5 +40,25 @@ export function CustomMainMenu() {
         </TldrawUiMenuGroup>
       </div>
     </DefaultMainMenu>
+  );
+}
+
+function CustomPreferencesGroup() {
+  return (
+    <TldrawUiMenuGroup id="preferences">
+      <TldrawUiMenuSubmenu id="preferences" label="menu.preferences">
+        <TldrawUiMenuGroup id="preferences-actions">
+          <ToggleSnapModeItem />
+          <ToggleToolLockItem />
+          <ToggleGridItem />
+          <ToggleWrapModeItem />
+          <ToggleFocusModeItem />
+          <ToggleEdgeScrollingItem />
+          <ToggleReduceMotionItem />
+        </TldrawUiMenuGroup>
+      </TldrawUiMenuSubmenu>
+      <LanguageMenu />
+      <KeyboardShortcutsMenuItem />
+    </TldrawUiMenuGroup>
   );
 }
